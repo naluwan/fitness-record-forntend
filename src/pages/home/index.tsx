deb10a350509600f8665912306c68e457c6f71c0
naluwan <street.baller06@gmail.com>
2023-04-24 23:55:33 +0800

    feat: update Home component and add react query to fetch records

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,38 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
 import FRHeader from 'components/FRHeader';
+import FRContainer from 'components/FRContainer';
+import { useQuery } from 'react-query';
+import { shallow } from 'zustand/shallow';
+import useRecordStore from 'store/useRecordStore';
+import { fetchRecords } from 'services/apis';
 
 const Home: React.FC = () => {
+  const { onSetRecords } = useRecordStore((state) => {
+    return {
+      onSetRecords: state.onSetRecords,
+    };
+  }, shallow);
+
+  const { data, isSuccess, isLoading, isError } = useQuery('allRecords', fetchRecords);
+
+  React.useEffect(() => {
+    if (isSuccess && !isLoading && !isError) {
+      console.log(data);
+    }
+  });
+
   return (
     <>
       <FRHeader />
-      <div>home</div>
+      <FRContainer>
+        <div className='flex lg:justify-center'>
+          {/* left */}
+          <div className='w-full lg:w-[600px]'>記錄</div>
+          {/* right */}
+          <div className='hidden lg:block lg:w-[424px]'>排行榜</div>
+        </div>
+      </FRContainer>
     </>
   );
 };
