4bcd9f6285d1ddddc21a64e58206a39e67277db9
naluwan <street.baller06@gmail.com>
2023-05-03 16:09:11 +0800

    feat: add prettier plugin tailwindcss and classnames

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,7 +3,9 @@ module.exports = {
   tabWidth: 2,
   printWidth: 100,
   singleQuote: true,
-  trailingComma: "all",
+  trailingComma: 'all',
   jsxSingleQuote: true,
   bracketSpacing: true,
+  plugins: [require('prettier-plugin-tailwindcss')],
+  tailwindConfig: './tailwind.config.js',
 };
