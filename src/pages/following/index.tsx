0be4d56897a570f5df74a25395cbcb481b784bd0
naluwan <street.baller06@gmail.com>
2023-05-04 19:08:59 +0800

    feat: add panelRef

--- src/pages/following/index.tsx
+++ src/pages/following/index.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
-import FRHeader from 'components/FRHeader';
+import FRHeader, { IRef } from 'components/FRHeader';
 import FRContainer from 'components/FRContainer';
+import { shallow } from 'zustand/shallow';
+import useRecordStore from 'store/useRecordStore';
 
 const Following: React.FC = () => {
+  const { onSetOpenPanel } = useRecordStore((state) => {
+    return {
+      onSetOpenPanel: state.onSetOpenPanel,
+    };
+  }, shallow);
+
+  // popover panel ref
+  const panelRef = React.useRef<IRef>(null);
+
+  // 畫面點擊時，如果element沒有包含在popoverRef底下的話，就關閉panel
+  window.addEventListener('mousedown', (e) => {
+    if (
+      panelRef.current &&
+      !panelRef.current.getDiv().contains(e.target as HTMLElement) &&
+      !panelRef.current.getButton().contains(e.target as HTMLElement)
+    ) {
+      onSetOpenPanel(false);
+    }
+  });
   return (
     <>
-      <FRHeader />
+      <FRHeader ref={panelRef} />
       <FRContainer>
         <div>following</div>
       </FRContainer>
