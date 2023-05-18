25a6824fc63f8c850b48f47533cc0b6c95d0db04
naluwan <street.baller06@gmail.com>
2023-05-18 15:33:10 +0800

    feat: import onSetIsDark function from useRecordStore and get isDark from localStorage to set darkmode

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,28 @@ import useRecordStore from 'store/useRecordStore';
 import routes from './routes';
 
 const App: React.FC = () => {
-  const { isAppInitializedComplete, init } = useRecordStore((state) => {
+  const { isAppInitializedComplete, onSetIsDark, init } = useRecordStore((state) => {
     return {
       isAppInitializedComplete: state.isAppInitializedComplete,
+      onSetIsDark: state.onSetIsDark,
       init: state.init,
     };
   }, shallow);
+
   React.useEffect(() => {
     if (!isAppInitializedComplete) {
       init();
     }
+    const isDarkMode = localStorage.getItem('isDark') || 'false';
+    if (isDarkMode === 'true') {
+      onSetIsDark(true);
+      document.documentElement.classList.add('dark');
+    } else {
+      onSetIsDark(false);
+      document.documentElement.classList.remove('dark');
+    }
   });
+
   const element = useRoutes(routes);
   return element;
 };
