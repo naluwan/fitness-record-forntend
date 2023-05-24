dbcf94f259ac6aef0fca49ed190e609d2ad02898
naluwan <street.baller06@gmail.com>
2023-05-24 14:25:50 +0800

    feat: add close button on modal

--- src/components/FRModal.tsx
+++ src/components/FRModal.tsx
@@ -14,8 +14,8 @@ const FRModal: React.FC<FRModalProps> = (props) => {
     <div
       onClick={onClose}
       className={`fixed inset-0 flex items-center justify-center transition-colors ${
-        open ? 'visible bg-black/20' : 'invisible'
-      } dark:bg-black/70`}
+        open ? 'visible bg-black/20 dark:bg-black/70' : 'invisible'
+      }`}
       role='button'
       tabIndex={-1}
     >
@@ -27,6 +27,22 @@ const FRModal: React.FC<FRModalProps> = (props) => {
         onClick={(e) => e.stopPropagation()}
         role='contentinfo'
       >
+        {/* 關閉 X按鈕 */}
+        <button
+          className='absolute right-1 top-2 rounded-lg bg-white p-1 text-gray-400 hover:bg-gray-50 hover:text-gray-600 lg:right-2'
+          onClick={onClose}
+        >
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='h-6 w-6'
+          >
+            <path strokeLinecap='round' strokeLinejoin='round' d='M6 18L18 6M6 6l12 12' />
+          </svg>
+        </button>
         {children}
       </div>
     </div>
