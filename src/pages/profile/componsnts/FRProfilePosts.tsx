55be853215096bbeee11dccf4eaf3c783167e019
naluwan <street.baller06@gmail.com>
2023-06-29 17:53:23 +0800

    feat: add FRProfilePosts component to show posts on Profile page

---
+++ src/pages/profile/componsnts/FRProfilePosts.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const FRProfilePosts = () => {
+  return (
+    <div className='grid grid-cols-3 gap-1'>
+      <div className='max-w-[350px]'>
+        <img src='https://i.imgur.com/SOVduI2.jpg' className='h-full w-full' alt='' />
+      </div>
+      <div className='max-w-[350px]'>
+        <img src='https://i.imgur.com/SOVduI2.jpg' className='h-full w-full' alt='' />
+      </div>
+      <div className='max-w-[350px]'>
+        <img src='https://i.imgur.com/SOVduI2.jpg' className='h-full w-full' alt='' />
+      </div>
+      <div className='max-w-[350px]'>
+        <img src='https://i.imgur.com/SOVduI2.jpg' className='h-full w-full' alt='' />
+      </div>
+      <div className='max-w-[350px]'>
+        <img src='https://i.imgur.com/SOVduI2.jpg' className='h-full w-full' alt='' />
+      </div>
+      <div className='max-w-[350px]'>
+        <img src='https://i.imgur.com/SOVduI2.jpg' className='h-full w-full' alt='' />
+      </div>
+    </div>
+  );
+};
+
+export default FRProfilePosts;
