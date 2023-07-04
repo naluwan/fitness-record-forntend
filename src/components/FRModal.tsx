05eeed36c76fcb05237a01964b7b8222f8706762
naluwan <street.baller06@gmail.com>
2023-07-04 17:10:09 +0800

    feat: add target props for decide to pop window to which element and add currentPage props to set style of pop window

--- src/components/FRModal.tsx
+++ src/components/FRModal.tsx
@@ -1,35 +1,33 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 type FRModalProps = {
   open: boolean;
   onClose: () => void;
+  target?: boolean;
+  currentPage?: 'edit' | 'post' | 'profilePost' | 'FRUser';
   children: React.ReactNode;
 };
 
 const FRModal: React.FC<FRModalProps> = (props) => {
-  const { open, onClose, children } = props;
+  const { open, onClose, target, currentPage, children } = props;
 
-  return (
-    // backdrop
-    <div
-      onClick={onClose}
-      className={`fixed inset-0 z-50 flex items-center justify-center transition-colors ${
-        open ? 'visible bg-black/20 dark:bg-black/40' : 'invisible'
-      }`}
-      role='button'
-      tabIndex={-1}
-    >
-      {/* modal */}
+  return target ? (
+    ReactDOM.createPortal(
+      // backdrop
       <div
-        className={`mt-[70px] h-[80%] w-full cursor-default overflow-auto rounded-lg bg-white p-6 transition-all duration-500 dark:bg-black lg:h-auto lg:w-[924px] ${
-          open ? 'scale-100 opacity-100' : 'scale-125  opacity-0'
+        onClick={onClose}
+        className={`fixed inset-0 z-50 flex items-center justify-center transition-colors ${
+          open ? 'visible bg-black/20 dark:bg-black/40' : 'invisible'
         }`}
-        onClick={(e) => e.stopPropagation()}
-        role='contentinfo'
+        role='button'
+        tabIndex={-1}
       >
         {/* 關閉 X按鈕 */}
         <button
-          className='absolute right-1 top-2 rounded-full bg-white p-1 text-gray-400 hover:bg-gray-50 hover:text-gray-600 lg:right-2'
+          className={`absolute right-1 top-2 p-1 text-black duration-500 hover:scale-125 dark:text-white lg:right-2 ${
+            open ? 'opacity-100' : 'opacity-0'
+          }`}
           onClick={onClose}
         >
           <svg
@@ -38,11 +36,65 @@ const FRModal: React.FC<FRModalProps> = (props) => {
             viewBox='0 0 24 24'
             strokeWidth={2.5}
             stroke='currentColor'
-            className='h-6 w-6'
+            className='h-8 w-8'
           >
             <path strokeLinecap='round' strokeLinejoin='round' d='M6 18L18 6M6 6l12 12' />
           </svg>
         </button>
+        {/* modal */}
+        <div
+          className={`mt-[70px] h-[80%] w-full cursor-default overflow-auto rounded-lg  transition-all duration-500 lg:h-auto ${
+            open ? 'scale-100 opacity-100' : 'scale-125  opacity-0'
+          } ${(currentPage === 'edit' || currentPage === 'post') && 'p-6'}
+        ${currentPage === 'FRUser' ? 'h-auto lg:w-[40%]' : 'lg:w-[924px]'}
+        ${currentPage === 'FRUser' ? 'bg-[#1c1c1c]' : 'bg-white dark:bg-black'}
+        `}
+          onClick={(e) => e.stopPropagation()}
+          role='contentinfo'
+        >
+          {children}
+        </div>
+      </div>,
+      document.body,
+    ) // backdrop
+  ) : (
+    <div
+      onClick={onClose}
+      className={`fixed inset-0 z-50 flex items-center justify-center transition-colors ${
+        open ? 'visible bg-black/20 dark:bg-black/40' : 'invisible'
+      }`}
+      role='button'
+      tabIndex={-1}
+    >
+      {/* 關閉 X按鈕 */}
+      <button
+        className={`absolute right-1 top-2 p-1 text-black duration-500 hover:scale-125 dark:text-white lg:right-2 ${
+          open ? 'opacity-100' : 'opacity-0'
+        }`}
+        onClick={onClose}
+      >
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
+          strokeWidth={2.5}
+          stroke='currentColor'
+          className='h-8 w-8'
+        >
+          <path strokeLinecap='round' strokeLinejoin='round' d='M6 18L18 6M6 6l12 12' />
+        </svg>
+      </button>
+      {/* modal */}
+      <div
+        className={`mt-[70px] h-[80%] w-full cursor-default overflow-auto rounded-lg  transition-all duration-500 lg:h-auto  ${
+          open ? 'scale-100 opacity-100' : 'scale-125  opacity-0'
+        } ${(currentPage === 'edit' || currentPage === 'post') && 'p-6'}
+        ${currentPage === 'FRUser' ? 'h-auto lg:w-[40%]' : 'lg:w-[924px]'}
+        ${currentPage === 'FRUser' ? 'bg-[#1c1c1c]' : 'bg-white dark:bg-black'}
+        `}
+        onClick={(e) => e.stopPropagation()}
+        role='contentinfo'
+      >
         {children}
       </div>
     </div>
