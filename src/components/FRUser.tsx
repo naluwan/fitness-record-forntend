934924a26a433586226e91d67ff854cae5430fb1
naluwan <street.baller06@gmail.com>
2023-07-04 17:39:18 +0800

    feat: add target props and import FRModal to update more popover panel

--- src/components/FRUser.tsx
+++ src/components/FRUser.tsx
@@ -19,6 +19,7 @@ type FRUserProps = {
   userId?: number;
   recordId?: number;
   showMore?: boolean;
+  target?: boolean;
   onRefetch?: () => void;
   onRefetchRank?: () => void;
 };
@@ -33,6 +34,7 @@ const FRUser: React.FC<FRUserProps> = (props) => {
     avatar,
     userId,
     recordId,
+    target,
     showMore = false,
     onRefetch,
     onRefetchRank,
@@ -161,7 +163,7 @@ const FRUser: React.FC<FRUserProps> = (props) => {
       <Link
         className={`${
           size === 'small' ? 'h-[40px] w-[40px]' : 'h-[60px] w-[60px]'
-        } overflow-hidden rounded-full`}
+        } flex-shrink-0 overflow-hidden rounded-full`}
         style={{
           backgroundImage: `url(${avatar})`,
           backgroundPosition: 'center',
@@ -193,77 +195,6 @@ const FRUser: React.FC<FRUserProps> = (props) => {
               />
             </svg>
           </button>
-
-          {/* more popover panel */}
-          <div
-            className={`lg:w-[250px]; duration-5000 absolute right-0 top-8 z-50 w-[110px] rounded-lg border  border-[#efefef] bg-white p-2 shadow-lg transition ease-in-out dark:bg-[#262626] ${
-              openPanel ? 'block' : 'hidden'
-            } lg:left-7 lg:top-0`}
-            ref={panelRef}
-          >
-            {/* 編輯 */}
-            <div className='mb-1 rounded-lg p-1 hover:bg-[#e6e6e6] dark:hover:bg-[#1c1c1c]'>
-              <button
-                className='flex w-full items-center'
-                onClick={() => atEditRecord(recordId as number)}
-              >
-                {/* left icon */}
-                <div className='bg-fb-input flex items-center justify-center rounded-full p-1'>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    fill='none'
-                    viewBox='0 0 24 24'
-                    strokeWidth={1.5}
-                    stroke='currentColor'
-                    className='h-4 w-4'
-                  >
-                    <path
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                      d='M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10'
-                    />
-                  </svg>
-                </div>
-
-                {/* right text */}
-                <div className='flex-1'>
-                  <p className='mb-[1px] text-base text-black dark:text-white'>編輯</p>
-                </div>
-              </button>
-            </div>
-
-            {/* 刪除 */}
-            <div className='mb-1 rounded-lg p-1 hover:bg-[#e6e6e6] dark:hover:bg-[#1c1c1c]'>
-              <button
-                className='flex w-full items-center'
-                onClick={() => atDeleteRecord(recordId as number)}
-                disabled={isFetching}
-              >
-                {/* left icon */}
-                <div className='bg-fb-input flex items-center justify-center rounded-full p-1'>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    fill='none'
-                    viewBox='0 0 24 24'
-                    strokeWidth={1.5}
-                    stroke='currentColor'
-                    className='h-4 w-4'
-                  >
-                    <path
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                      d='M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0'
-                    />
-                  </svg>
-                </div>
-
-                {/* right text */}
-                <div className='flex-1'>
-                  <p className='mb-[1px] text-base text-red-600'>刪除</p>
-                </div>
-              </button>
-            </div>
-          </div>
         </div>
       )}
       {showFollow && (
@@ -274,7 +205,7 @@ const FRUser: React.FC<FRUserProps> = (props) => {
         </button>
       )}
       {editRecord && newRecord && (
-        <FRModal open={editing} onClose={() => atClickCloseBtn()}>
+        <FRModal target open={editing} currentPage='edit' onClose={() => atClickCloseBtn()}>
           <FRPostFrom
             record={newRecord}
             onSetNewRecord={setNewRecord as (record: React.SetStateAction<Record>) => void}
@@ -285,6 +216,42 @@ const FRUser: React.FC<FRUserProps> = (props) => {
           />
         </FRModal>
       )}
+
+      {/* more popover panel */}
+      <FRModal
+        currentPage='FRUser'
+        open={openPanel}
+        onClose={() => setOpenPanel(false)}
+        target={target}
+      >
+        {/* 編輯 */}
+        <div className='border-b-2 border-[#262626] py-2 first:pt-4 last:pb-4'>
+          <button
+            className='flex w-full items-center'
+            onClick={() => {
+              setOpenPanel(false);
+              atEditRecord(recordId as number);
+            }}
+          >
+            <div className='flex-1'>
+              <p className='mb-[1px] text-base text-white'>編輯記錄</p>
+            </div>
+          </button>
+        </div>
+
+        {/* 刪除 */}
+        <div className='py-4 first:pb-2 last:pt-2'>
+          <button
+            className='flex w-full items-center'
+            onClick={() => atDeleteRecord(recordId as number)}
+            disabled={isFetching}
+          >
+            <div className='flex-1'>
+              <p className='mb-[1px] text-base text-red-600'>刪除記錄</p>
+            </div>
+          </button>
+        </div>
+      </FRModal>
     </div>
   );
 };
