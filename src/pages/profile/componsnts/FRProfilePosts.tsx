cfb0d224fb43410a89b2c6b58b8ef72aceeab403
naluwan <street.baller06@gmail.com>
2023-07-04 17:43:27 +0800

    feat: update FRProfilePost for showing all reocrds of selected user

--- src/pages/profile/componsnts/FRProfilePosts.tsx
+++ src/pages/profile/componsnts/FRProfilePosts.tsx
@@ -1,28 +1,40 @@
 import React from 'react';
+import { Images, Record } from 'types';
 
-const FRProfilePosts = () => {
+type FRProfilePostsProps = {
+  records: Record[];
+  onSetOpenModal: (open: boolean) => void;
+  onSetSelectedRecord: (record: Record) => void;
+};
+
+const FRProfilePosts: React.FC<FRProfilePostsProps> = (props) => {
+  const { records, onSetOpenModal, onSetSelectedRecord } = props;
+
   return (
     <div className='grid grid-cols-3 gap-1'>
-      <div className='max-w-[350px]'>
-        <img src='https://i.imgur.com/SOVduI2.jpg' className='h-full w-full' alt='' />
-      </div>
-      <div className='max-w-[350px]'>
-        <img src='https://i.imgur.com/SOVduI2.jpg' className='h-full w-full' alt='' />
-      </div>
-      <div className='max-w-[350px]'>
-        <img src='https://i.imgur.com/SOVduI2.jpg' className='h-full w-full' alt='' />
-      </div>
-      <div className='max-w-[350px]'>
-        <img src='https://i.imgur.com/SOVduI2.jpg' className='h-full w-full' alt='' />
-      </div>
-      <div className='max-w-[350px]'>
-        <img src='https://i.imgur.com/SOVduI2.jpg' className='h-full w-full' alt='' />
-      </div>
-      <div className='max-w-[350px]'>
-        <img src='https://i.imgur.com/SOVduI2.jpg' className='h-full w-full' alt='' />
-      </div>
+      {records.length &&
+        records.map((record) => {
+          const images = record.Images as Images[];
+          return (
+            <button
+              key={record.id}
+              className='group relative aspect-1 max-h-[350px] max-w-[350px]'
+              onClick={() => {
+                onSetSelectedRecord(record);
+                onSetOpenModal(true);
+              }}
+            >
+              <div className='absolute left-0 top-0 z-20 hidden h-full w-full bg-black/20 group-hover:block' />
+              <img
+                src={images.length ? images[0].url : ''}
+                className='h-full w-full object-cover'
+                alt=''
+              />
+            </button>
+          );
+        })}
     </div>
   );
 };
 
-export default FRProfilePosts;
+export default React.memo(FRProfilePosts);
