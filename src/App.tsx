f13acd6ef870d7891e3d3d40c984b347fa8cd53d
naluwan <street.baller06@gmail.com>
2023-08-07 14:10:42 +0800

    feat: add routes of pages and connect socket.io when user open this web

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,57 @@ import { useRoutes } from 'react-router-dom';
 import React from 'react';
 import { shallow } from 'zustand/shallow';
 import useRecordStore from 'store/useRecordStore';
+import type { State } from 'store/useRecordStore';
 import { useQuery } from 'react-query';
-import routes from './routes';
+import type { RouteObject } from 'react-router-dom';
+import SignIn from 'pages/signIn';
+import Register from 'pages/register';
+import LineLogin from 'pages/signIn/components/LineLogin';
+import Profile from 'pages/profile';
+import { io } from 'socket.io-client';
+import Ranking from './pages/ranking';
+import Home from './pages/home';
+import NotFound from './pages/notFound';
 
 const App: React.FC = () => {
-  const { onSetIsDark, init } = useRecordStore((state) => {
-    return {
-      onSetIsDark: state.onSetIsDark,
-      init: state.init,
-    };
-  }, shallow);
+  // 實作socket.io
+  const socket = io('http://localhost:3000', {
+    transports: ['websocket'],
+  });
+
+  const { init, onSetIsDark, onSetNeedUpdateRanking, onSetSocket } = useRecordStore(
+    (state: State) => {
+      return {
+        init: state.init,
+        onSetIsDark: state.onSetIsDark,
+        onSetNeedUpdateRanking: state.onSetNeedUpdateRanking,
+        onSetSocket: state.onSetSocket,
+      };
+    },
+    shallow,
+  );
 
   // 只要進到這個網站，就會檢查token是否有效
   useQuery('auth', init);
+
+  React.useEffect(() => {
+    // 進入頁面連接socket.io
+    socket.connect();
+    onSetSocket(socket);
+    // 監聽updateRanking頻道
+    socket.on('updateRanking', (needUpdate) => {
+      onSetNeedUpdateRanking(needUpdate);
+    });
+
+    socket.on('disConnect', (msg) => {
+      console.log('from server disConnect socket ===> ', msg);
+    });
+
+    // 離開頁面斷開socket.io連線
+    return () => {
+      socket.close();
+    };
+  }, [socket, onSetNeedUpdateRanking, onSetSocket]);
 
   React.useEffect(() => {
     const isDarkMode = localStorage.getItem('isDark') || 'false';
@@ -27,6 +65,44 @@ const App: React.FC = () => {
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const routes: RouteObject[] = [
+    {
+      path: '/',
+      element: <Home />,
+      children: [],
+    },
+    {
+      path: '/ranking',
+      element: <Ranking />,
+      children: [],
+    },
+    {
+      path: '/signin/line',
+      element: <LineLogin />,
+      children: [],
+    },
+    {
+      path: '/signin',
+      element: <SignIn />,
+      children: [],
+    },
+    {
+      path: '/register',
+      element: <Register />,
+      children: [],
+    },
+    {
+      path: '/profile/:userId',
+      element: <Profile />,
+      children: [],
+    },
+    {
+      path: '*',
+      element: <NotFound />,
+      children: [],
+    },
+  ];
 
   const element = useRoutes(routes);
   return element;
